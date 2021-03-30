66e01f94c91f215ef38a69f13ffc43520eb82cea
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-30 13:38:13 +0100

    Primer planteamiento

--- practica_6/src/ejercicio-2/isConvertible.ts
+++ practica_6/src/ejercicio-2/isConvertible.ts
@@ -1,61 +1,91 @@
 
 //La unidad fundamental para medir el tiempo es en segundos
-export enum TUnits{
+export enum TUnits {
   minutes = 60,
   hours = 3600,
   day = 86400,
   week = 604800,
   month = 2592000,
-  year = 31536000
+  year = 31536000,
+  
 }
 
 /**
  *  ### Interfaz genérica que hace la conversión de tiempo
  */
 
-export interface isConvertible<T, U>{
+export interface isConvertible<T>{
 // hacer una funcion que en función de la medida que se le pase saque una conversión u otra
 /**
  * name
  */
-  public convertirStoMinutes(numMinutes: TUnits) {
+  /*public convertirStoMinutes(numMinutes: TUnits) {
     let result = numMinutes*minutes;
 	  return console.log(`${numMinutes} minutos son ${result} segundos`)
-  } 
+  } */
 }
 
 
-
-export abstract class Magnitude<TUnits> implements isConvertible<TUnits, TUnits> {
-  constructor(private value: number) { 
+export abstract class Magnitude<T> implements isConvertible<T> {
+  constructor(private value: T[]) {
   }
 
-  getMagnitude() {
-    return this.value;
-  }
-  
-  setMagnitude(value: number) {
-    this.value = value;
+  addMagnitude(newMagnitude: T) {
+    this.value.push(newMagnitude);
   }
 
-  
+  getNumberOfMagnitude() {
+    return this.value.length;
+  }
+
+  abstract print(): void;
+
 }
+
+
 
 export class MagnitudeTime<TUnits> extends Magnitude<TUnits>{
-  constructor(protected valueTime: number) { 
+  constructor(protected valueTime: TUnits[], private amount: TUnits, private unit: string) { 
     super(valueTime)
   }
 
   getMagnitude() {
     return this.valueTime;
   }
-  
-  setMagnitude(valueTime: number) {
-    this.valueTime = valueTime;
+
+  getAmount(){
+    return this.amount;
   }
+
+  getUnit(){
+    return this.unit;
+  }
+
+  addMagnitude(newMagnitude: TUnits) {
+    this.valueTime.push(newMagnitude);
+  }
+  
+  addAmount(nAmount: TUnits){
+    for(var i = 0; i < this.valueTime.length; i++){
+      this.valueTime[0] = nAmount;
+    }
+  }
+
+  addUnit(nUnit: TUnits){
+    for(var i = 0; i < this.valueTime.length; i++){
+      this.valueTime[1] = nUnit;
+    }
+  }
+
+  print() {
+    console.log(`${this.getAmount()} time, is ${this.getMagnitude()} seconds`);
+  }
+  
+  
 }
 
-const myConvertion = new MagnitudeTime([
-  new Rectangle('RedRectangle', 'red', 10, 5),
-  new Rectangle('YellowRectangle', 'yellow'),
-]);
+const myConvertion = new MagnitudeTime<TUnit>([],2, 'minutos');
+
+for(var i = 0; i < myConvertion.getAmount(); i++){
+  myConvertion.getAmount().print();
+}
--- practica_6/tests/ejercicio-2.spec.ts
+++ practica_6/tests/ejercicio-2.spec.ts
@@ -1,3 +1,5 @@
 import 'mocha';
 import {expect} from 'chai';
-import {isConvertible} from '../src/ejercicio-2/e2';
+import {isConvertible} from '../src/ejercicio-2/isConvertible';
+import {Magnitude} from '../src/ejercicio-2/isConvertible';
+import {MagnitudeTime} from '../src/ejercicio-2/isConvertible';
